fdeda85a7065a9face4761a8fae957a5353af8c5
Karan Sivarat <bomb0069@gmail.com>
2014-06-09 11:37:31 -0400

    add FirstTest

---
+++ src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
@@ -0,0 +1,12 @@
+package th.own.bomb0069.kata;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class RomanNumberTest {
+	
+	@Test
+	public void testFirst (){
+		assertTrue(true);
+	}
+}
