9dc1304df8b3c020b962251919408c9877f1c523
Karan Sivarat <bomb0069@gmail.com>
2014-06-09 11:45:00 -0400

    test1ShoulBeReturnI

---
+++ src/main/java/th/own/bomb0069/kata/RomanNumber.java
@@ -0,0 +1,9 @@
+package th.own.bomb0069.kata;
+
+public class RomanNumber {
+
+	public String get(int number) {
+		return "I";
+	}
+	
+}
--- src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
+++ src/test/java/th/own/bomb0069/kata/RomanNumberTest.java
@@ -1,12 +1,14 @@
 package th.own.bomb0069.kata;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class RomanNumberTest {
 	
 	@Test
-	public void testFirst (){
-		assertTrue(true);
-	}
+	public void test1ShouldBeReturnI (){
+		RomanNumber romanNumber = new RomanNumber();
+		String actualString = romanNumber.get(1);
+		assertEquals("I", actualString);
+	} 
 }
