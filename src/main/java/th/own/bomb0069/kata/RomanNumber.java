d7dbcfa47eef445b868793f1247e07c3d0949af6
Karan Sivarat <bomb0069@gmail.com>
2014-06-09 13:42:12 -0400

    refactor remove List and Change Map to LinkedMap for got sequence of key

--- src/main/java/th/own/bomb0069/kata/RomanNumber.java
+++ src/main/java/th/own/bomb0069/kata/RomanNumber.java
@@ -1,28 +1,25 @@
 package th.own.bomb0069.kata;
 
-import java.util.List;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class RomanNumber {
 
-	List<Integer> romanNumbers = Arrays.asList(1000,500,100,50,10,5,1);
-	static Map<Integer,String> romanChar = new HashMap<Integer,String>();
+	static Map<Integer,String> romanChar = new LinkedHashMap<Integer,String>();
 	static {
-		romanChar.put(1,"I");
-		romanChar.put(5,"V");
-		romanChar.put(10,"X");
-		romanChar.put(50,"L");
-		romanChar.put(100,"C");
-		romanChar.put(500,"D");
 		romanChar.put(1000,"M");
+		romanChar.put(500,"D");
+		romanChar.put(100,"C");
+		romanChar.put(50,"L");
+		romanChar.put(10,"X");
+		romanChar.put(5,"V");
+		romanChar.put(1,"I");
 	}
 
 	public String get(int number) {
 		String romanNumber = "";
 
-		for (Integer romanNo : romanNumbers) {
+		for (Integer romanNo : romanChar.keySet()) {
 			while ((number != 0) && (number >= romanNo - 1)) {
 				if (number == romanNo - 1) {
 					romanNumber+= "I" + romanChar.get(romanNo);
